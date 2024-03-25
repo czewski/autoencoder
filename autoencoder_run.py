375400289bcfef8b3a26ad92915083fa6ed4c99b
eduardo <eduardohb10@hotmail.com>
2024-03-25 11:21:42 -0300

    fix: more debug for autoencoder

--- autoencoder_run.py
+++ autoencoder_run.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 #Utils
@@ -9,19 +10,20 @@ import argparse
 from tqdm import tqdm
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 # from sklearn.preprocessing import LabelEncoder
 # import numpy as np
 # import pandas as pd
 
 # Local
 from models import autoencoder
-from utils import dataset, utils
+from utils import dataset, utils, metric
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_path', default='data/diginetica/', help='dataset directory path: datasets/diginetica/yoochoose1_4/yoochoose1_64')
-parser.add_argument('--batch_size', type=int, default=256, help='input batch size')
-parser.add_argument('--epoch', type=int, default=20, help='the number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.01, help='learning rate')  
+parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
+parser.add_argument('--epoch', type=int, default=5, help='the number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')  
 #parser.add_argument('--lr_dc', type=float, default=0.1, help='learning rate decay rate')
 #parser.add_argument('--lr_dc_step', type=int, default=80, help='the number of steps after which the learning rate decay') 
 #parser.add_argument('--topk', type=int, default=20, help='number of top score items selected for calculating recall and mrr metrics')
@@ -51,6 +53,7 @@ def main():
         sum_epoch_loss = 0
         start = time.time()
         log_aggr = 100
+        
         # Iterate over batches in the training data loader
         for i, (seq, lens) in tqdm(enumerate(train_loader)):
             seq = seq.to(device).to(torch.float32)
@@ -70,13 +73,13 @@ def main():
             iter_num = epoch * len(train_loader) + i + 1
         
             if i % log_aggr == 0:
-                print("outputs")
-                print(outputs)
+                # print("outputs")
+                # print(outputs)
 
-                print("seq")
-                print(seq)
+                # print("seq")
+                # print(seq)
 
-                #time.sleep(5)
+                # time.sleep(5)
                 print('[TRAIN] epoch %d/%d batch loss: %.4f (avg %.4f) (%.2f im/s)'% (epoch + 1, args.epoch, loss_val, sum_epoch_loss / (i + 1),len(seq) / (time.time() - start)))
 
             start = time.time()
@@ -86,11 +89,23 @@ def main():
         epoch_loss = sum_epoch_loss/len(train_loader)
         losses.append(epoch_loss)
 
+        #recall, mrr, hit = validate(valid_loader_fold, model)
+        #print('Epoch {} validation: Recall@{}: {:.4f}, MRR@{}: {:.4f}, HIT@{}: {:.4f} \n'.format(epoch, args.topk, recall, args.topk, mrr, args.topk, hit))
+
+        # store best loss and save a model checkpoint
+        #ckpt_dict = {
+        #     'epoch': epoch + 1,
+        #     'state_dict': model.state_dict(),
+        #     'optimizer': optimizer.state_dict()
+        # }
+
+        #torch.save(ckpt_dict, 'autoencoder_latest_checkpoint.pth.tar')
+
     # Loss curve
     print('--------------------------------')
     print('Plotting loss curve...')
     plt.clf()
-    plt.plot(losses)
+    plt.plot(losses[1:])
     plt.title('Training Loss Over Epochs')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')  
@@ -98,3 +113,24 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def validate(valid_loader, model):
+    model.eval()
+    recalls = []
+    mrrs = []
+    hits = []
+    with torch.no_grad():
+        for seq, target, lens in tqdm(valid_loader):
+            seq = seq.to(device)
+            target = target.to(device)
+            outputs = model(seq, lens)
+            logits = F.softmax(outputs, dim = 1)
+            recall, mrr, hit = metric.evaluate(logits, target, k = args.topk)
+            recalls.append(recall)
+            mrrs.append(mrr)
+            hits.append(hit)
+    
+    mean_recall = np.mean(recalls)
+    mean_mrr = np.mean(mrrs)
+    mean_hit = np.mean(hits)
+    return mean_recall, mean_mrr, mean_hit
--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,19 +1,19 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import torch.nn.init as weight_init
 
 class AutoEncoder(nn.Module):
     def __init__(self, num_items, embedding_dim=100, hidden_dim=100):
         super(AutoEncoder, self).__init__()
-
-
+        
         #Method 3
         # Stack encoders and decoders
         encoder_modules, decoder_modules = [], []
-        encoder_modules.append(nn.Linear(19,256))
-        encoder_modules.append(nn.Linear(256,100))
-        decoder_modules.append(nn.Linear(100,256))
-        decoder_modules.append(nn.Linear(256,19))
+        encoder_modules.append(nn.Linear(19,100))
+        #encoder_modules.append(nn.Linear(256,100))
+        decoder_modules.append(nn.Linear(100,19))
+        #decoder_modules.append(nn.Linear(256,19))
         self.encoder = nn.ModuleList(encoder_modules)
         self.decoder = nn.ModuleList(decoder_modules)
 
@@ -41,11 +41,25 @@ class AutoEncoder(nn.Module):
         #Method 1
         # self.encoder = nn.Linear(19, hidden_dim)  # Adjusted encoder input size
         # self.decoder = nn.Linear(hidden_dim, 19)  # Adjusted decoder output size
+
+        #Initialize weights to ONE
+        # for layer in self.encoder:
+        #     if isinstance(layer, nn.Linear):
+        #         weight_init.constant_(layer.weight, 1)
+        #         weight_init.constant_(layer.bias, 0)  
+        
+        # #Initialize weights to ONE
+        # for layer in self.decoder:
+        #     if isinstance(layer, nn.Linear):
+        #         weight_init.constant_(layer.weight, 1)
+        #         weight_init.constant_(layer.bias, 0)  
     
     def weight_init(self, m):
         if isinstance(m, nn.Linear):
             nn.init.kaiming_normal_(m.weight)
             m.bias.data.zero_()
+
+
 
     def forward(self, x):
         #Method 3
@@ -73,23 +87,3 @@ class AutoEncoder(nn.Module):
         # #decoded = torch.sigmoid(self.decoder(encoded))  # Pass through decoder with sigmoid activation
         # decoded = torch.softmax(self.decoder(encoded), dim=1)  # Pass through decoder with sigmoid activation
         return x
-
-
-# Definir o modelo de autoencoder com grus
-# class AutoEncoder(nn.Module):
-#     def __init__(self, num_items, embedding_dim=100, hidden_dim=200, batch_size=50):
-#         super(AutoEncoder, self).__init__()
-#         #self.embedding = nn.Embedding(num_items + 1, embedding_dim)
-#         #self.encoder = nn.GRU(embedding_dim, hidden_dim, batch_first=True)
-#         self.encoder = nn.Linear(19, 50)
-#         self.decoder = nn.Linear(19, batch_size)
-#         #self.decoder = nn.Linear(hidden_dim, num_items)
-        
-#     def forward(self, input):
-#         #embedded = self.embedding(input)
-#         #_, hidden = self.encoder(embedded)
-#         input = input.to(torch.float32).transpose(0, 1)
-#         _, hidden = self.encoder(input)
-#         output = self.decoder(hidden.squeeze(0))
-#         logits = F.sigmoid(output)
-#         return logits
