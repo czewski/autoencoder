feedb5831e5bfdb4cb04bb6be3cc3ad543644c34
eduardo <eduardohb10@hotmail.com>
2024-05-21 19:18:44 -0300

    fix: autoencoder progress (actually working)

--- autoencoder_run.py
+++ autoencoder_run.py
@@ -11,25 +11,22 @@ from tqdm import tqdm
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-# from sklearn.preprocessing import LabelEncoder
-# import numpy as np
-# import pandas as pd
 
 # Local
 from models import autoencoder
 from utils import dataset, utils, metric
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset_path', default='data/diginetica/', help='dataset directory path: datasets/diginetica/yoochoose1_4/yoochoose1_64')
-parser.add_argument('--batch_size', type=int, default=64, help='input batch size')
-parser.add_argument('--epoch', type=int, default=200, help='the number of epochs to train for')
+parser.add_argument('--dataset_path', default='data/diginetica_normalized/', help='dataset directory path: datasets/diginetica/yoochoose1_4/yoochoose1_64')
+parser.add_argument('--batch_size', type=int, default=128, help='input batch size')
+parser.add_argument('--epoch', type=int, default=50, help='the number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')  
 #parser.add_argument('--lr_dc', type=float, default=0.1, help='learning rate decay rate')
 #parser.add_argument('--lr_dc_step', type=int, default=80, help='the number of steps after which the learning rate decay') 
 #parser.add_argument('--topk', type=int, default=20, help='number of top score items selected for calculating recall and mrr metrics')
 args = parser.parse_args()
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def main():
     torch.manual_seed(42)
@@ -37,17 +34,12 @@ def main():
 
     train_data = dataset.RecSysDataset(train)
     test_data = dataset.RecSysDataset(test)
-    train_loader = DataLoader(train_data, batch_size = args.batch_size, shuffle = True, collate_fn = utils.collate_fn)
-    test_loader = DataLoader(test_data, batch_size = args.batch_size, shuffle = False, collate_fn = utils.collate_fn)
+    train_loader = DataLoader(train_data, batch_size = args.batch_size, shuffle = True) #, collate_fn = utils.collate_fn
+    test_loader = DataLoader(test_data, batch_size = args.batch_size, shuffle = False) #, collate_fn = utils.collate_fn
 
-    n_items = 43098
-    model = autoencoder.AutoEncoder().to(device)
-
-    optimizer = optim.Adam(model.parameters(), args.lr)
-    #optimizer = optim.RMSprop(model.parameters(), args.lr)
-    criterion = nn.MSELoss() #nn.KLDivLoss() # #nn.CrossEntropyLoss()    
-    #criterion = nn.BCELoss()
-
+    model = autoencoder.AutoEncoder().to(device) #    n_items = 43098
+    optimizer = optim.Adam(model.parameters(), args.lr) #optim.RMSprop
+    criterion = nn.MSELoss() #nn.KLDivLoss() nn.CrossEntropyLoss() nn.BCELoss()
 
     losses = []
     for epoch in tqdm(range(args.epoch)):
@@ -55,51 +47,37 @@ def main():
         sum_epoch_loss = 0
         start = time.time()
         log_aggr = 100
-        
-        # Iterate over batches in the training data loader
-        for i, (seq, lens) in tqdm(enumerate(train_loader)):
-            seq = seq.to(device).to(torch.float32)
 
+        for i, seq  in tqdm(enumerate(train_loader)):
+            seq = seq.to(device).to(torch.float32)
             optimizer.zero_grad()
             outputs = model(seq)
-            #print(outputs.size())
 
             #Create mask max=20, real=lens[x]
             #mask = utils.create_matrix_mask(len(seq), 20, lens)
-
-            #Apply mask
             #outputs = outputs * mask
 
-            loss = criterion(seq, outputs)
+            loss = criterion(outputs, seq)
             loss.backward()
             optimizer.step() 
 
-            # Get the numerical value of the loss function
             loss_val = loss.item()
             sum_epoch_loss += loss_val
 
             # Calculate the current iteration number
-            iter_num = epoch * len(train_loader) + i + 1
+            #iter_num = epoch * len(train_loader) + i + 1
         
             if i % log_aggr == 0:
-                # print("outputs")
-                # print(outputs)
-
-                # print("seq")
-                # print(seq)
-
-                # time.sleep(5)
                 print('[TRAIN] epoch %d/%d batch loss: %.4f (avg %.4f) (%.2f im/s)'% (epoch + 1, args.epoch, loss_val, sum_epoch_loss / (i + 1),len(seq) / (time.time() - start)))
 
             start = time.time()
-
 
         # Calculate the average loss for the epoch
         epoch_loss = sum_epoch_loss/len(train_loader)
         losses.append(epoch_loss)
 
-        #recall, mrr, hit = validate(valid_loader_fold, model)
-        #print('Epoch {} validation: Recall@{}: {:.4f}, MRR@{}: {:.4f}, HIT@{}: {:.4f} \n'.format(epoch, args.topk, recall, args.topk, mrr, args.topk, hit))
+        # recall, mrr, hit = validate(test_loader, model)
+        # print('Epoch {} validation: Recall@{}: {:.4f}, MRR@{}: {:.4f}, HIT@{}: {:.4f} \n'.format(epoch, args.topk, recall, args.topk, mrr, args.topk, hit))
 
         # store best loss and save a model checkpoint
         #ckpt_dict = {
@@ -122,24 +100,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-def validate(valid_loader, model):
-    model.eval()
-    recalls = []
-    mrrs = []
-    hits = []
-    with torch.no_grad():
-        for seq, target, lens in tqdm(valid_loader):
-            seq = seq.to(device)
-            target = target.to(device)
-            outputs = model(seq, lens)
-            logits = F.softmax(outputs, dim = 1)
-            recall, mrr, hit = metric.evaluate(logits, target, k = args.topk)
-            recalls.append(recall)
-            mrrs.append(mrr)
-            hits.append(hit)
-    
-    mean_recall = np.mean(recalls)
-    mean_mrr = np.mean(mrrs)
-    mean_hit = np.mean(hits)
-    return mean_recall, mean_mrr, mean_hit
--- models/autoencoder.py
+++ models/autoencoder.py
@@ -7,118 +7,24 @@ class AutoEncoder(nn.Module):
     def __init__(self, input_dim=20, hidden_dim=50):
         super(AutoEncoder, self).__init__()
         
-        #Method 5 (LSTM)
-        self.lstm = nn.LSTM(input_size=1, hidden_size=hidden_dim, num_layers=1, batch_first=False)
+        self.encoder = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, batch_first=True)
+        self.decoder = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
+        self.output_layer = nn.Linear(hidden_dim, input_dim)
+        self.relu = nn.ReLU()
 
-        #Method 4
-        #self.encoder = nn.Linear(input_dim, hidden_dim)
-        #self.decoder = nn.Linear(hidden_dim, input_dim)
-
-        #Method 3 -- Default one 
-        encoder_modules, decoder_modules = [], []
-        encoder_modules.append(nn.Linear(hidden_dim,hidden_dim))
-        decoder_modules.append(nn.Linear(hidden_dim,input_dim))
-        self.encoder = nn.ModuleList(encoder_modules)
-        self.decoder = nn.ModuleList(decoder_modules)
-
-        # Initialize weights
         self.initialize_weights()
-        #self.encoder.apply(self.weight_init)
-        #self.decoder.apply(self.weight_init)
 
-        #Method 2
-        # Encoder layers
-        # self.encoder = nn.Sequential(
-        #     nn.Linear(19, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, hidden_dim),
-        #     nn.ReLU()
-        # )
-
-        # # Decoder layers
-        # self.decoder = nn.Sequential(
-        #     nn.Linear(hidden_dim, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 19),
-        #     nn.Softmax() 
-        # )
-
-        #Method 1
-        # self.encoder = nn.Linear(19, hidden_dim)  # Adjusted encoder input size
-        # self.decoder = nn.Linear(hidden_dim, 19)  # Adjusted decoder output size
-
-        #Initialize weights to ONE
-        # for layer in self.encoder:
-        #     if isinstance(layer, nn.Linear):
-        #         weight_init.constant_(layer.weight, 1)
-        #         weight_init.constant_(layer.bias, 0)  
-        
-        # #Initialize weights to ONE
-        # for layer in self.decoder:
-        #     if isinstance(layer, nn.Linear):
-        #         weight_init.constant_(layer.weight, 1)
-        #         weight_init.constant_(layer.bias, 0)  
-    
     def initialize_weights(self):
         for layer in self.modules():
             if isinstance(layer, nn.Linear):
                 nn.init.xavier_uniform_(layer.weight)
                 nn.init.constant_(layer.bias, 0.0)
 
-    # def weight_init(self, m):
-    #     if isinstance(m, nn.Linear):
-    #         nn.init.kaiming_normal_(m.weight)
-    #         m.bias.data.zero_()
-
-
-
     def forward(self, x):
-        #Method 5 
+        encoded, _ = self.encoder(x)
+        encoded = self.relu(encoded)
 
-        #x = x.transpose(1,0)
-        x = x.unsqueeze(-1)
-        lstm_out, _ = self.lstm(x)
-        lstm_out = lstm_out[:, -1, :]
-        #print(lstm_out.size())
-
-        # Pass through Encoder
-        for i, layer in enumerate(self.encoder):
-            lstm_out = layer(lstm_out)
-            if i != len(self.encoder) - 1:
-                lstm_out = F.relu(lstm_out)
-
-        # Pass through Decoder
-        for i, layer in enumerate(self.decoder):
-            lstm_out = layer(lstm_out)
-            if i != len(self.decoder) - 1:
-                lstm_out = F.relu(lstm_out)
-
-        #Method 4
-        # x = torch.sigmoid(self.encoder(x))
-        # x = torch.sigmoid(self.decoder(x))
-
-        #Method 3
-        #x = F.normalize(x)
-        #x = self.input_dropout(x)
+        decoded, _ = self.decoder(encoded)
+        decoded = self.output_layer(decoded)
         
-        # for i, layer in enumerate(self.encoder):
-        #     x = layer(x)
-        #     if i != len(self.encoder) - 1:
-        #         x = F.relu(x)
-        #         #x = torch.tanh(x)
-
-        # for i, layer in enumerate(self.decoder):
-        #     x = layer(x)
-        #     if i != len(self.decoder) - 1:
-        #         x = F.relu(x)
-        #         #x = torch.tanh(x)
-        
-        #Method 2
-        # x = self.encoder(x)
-        # x = self.decoder(x)
-
-        #Method 1
-        # encoded = F.relu(self.encoder(input))  # Pass through encoder with ReLU activation
-        # #decoded = torch.sigmoid(self.decoder(encoded))  # Pass through decoder with sigmoid activation
-        # decoded = torch.softmax(self.decoder(encoded), dim=1)  # Pass through decoder with sigmoid activation
-        return lstm_out
+        return decoded
