b1757dbca20bc5f0929425a7b28b375d35365ec0
eduardo <eduardohb10@hotmail.com>
2024-08-06 18:33:25 -0300

    fix: load missing ids values into word2vec model

--- item2vec.py
+++ item2vec.py
@@ -6,27 +6,34 @@ import numpy as np
 import pickle
 from utils import dataset
 
-# Save/Train (?)
+## Train word2vec model
 root = "data/diginetica/"
 train, valid, test = dataset.load_data_narm(root, valid_portion=0.1, maxlen=15)
-
-# path_test_data = root + 'test.txt'
-# with open(path_test_data, 'rb') as f2:
-#     test_set = pickle.load(f2)
-#43098
-# 107312
-
 embedding_dim = 50
-
-# Train the item2vec model
 item2vec_model = Word2Vec(train[0], vector_size=embedding_dim, window=10, 
                           min_count=1, sg=1, seed=522, epochs=25)
-
-# Save the model if needed
 item2vec_model.save("embeddings/item2vec_05_08.model")
 
 
-## Load
+## Add missing ids to word2vec model
+# Assuming you have a Word2Vec model already trained
+# model = Word2Vec.load("embeddings/item2vec_05_08.model")
+# total_ids = set(range(43099))
+# trained_ids = set(int(key) for key in model.wv.key_to_index.keys())
+# missing_ids = total_ids - trained_ids
+
+# print(missing_ids)
+
+# vector_size = model.vector_size
+# for missing_id in missing_ids:
+#     random_vector = np.random.randn(vector_size)
+#     model.wv.add_vector(missing_id, random_vector)
+
+# # Save the updated model
+# model.save("embeddings/item2vec_06_08.model")
+
+
+## Create weight matrix
 # # Load the trained model
 # item2vec_model = Word2Vec.load("item2vec.model")
 
